aa8bcc85b9689fd885188e64a942c51b7f9643e5
tomocy <tomocy.dev@gmail.com>
2019-07-17 16:03:25 +0900

    add method for players to roll something rollable

--- player.go
+++ player.go
@@ -4,3 +4,11 @@ type Player struct {
 	Name       string
 	Characters []*Character
 }
+
+func (p *Player) Roll(rollable rollable) *Character {
+	return rollable.Roll()
+}
+
+type rollable interface {
+	Roll() *Character
+}
