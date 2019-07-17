3311d11ae6e3285fa2272d4231899cc9eab43f74
tomocy <tomocy.dev@gmail.com>
2019-07-17 15:59:19 +0900

    add method for gachas to roll

--- gacha.go
+++ gacha.go
@@ -1,6 +1,15 @@
 package goroneko
 
+import "math/rand"
+
 type Gacha struct {
 	Name       string
 	Characters []*Character
+}
+
+func (g *Gacha) Roll() *Character {
+	n := len(g.Characters)
+	x := rand.Intn(n)
+
+	return g.Characters[x]
 }
